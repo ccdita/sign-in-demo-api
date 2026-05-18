cce6fe70fc1f9499522e8917d7c95680a545a6c1
ccdita <christaldita@gmail.com>
2026-05-18 16:25:26 -0400

    Update Playwright configurations
    Update Playwright configurations based on testing preference.

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,34 +3,44 @@ import { defineConfig, devices } from '@playwright/test';
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
+ * In the terminal, enter: npm install dotenv
  */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './tests',
+  testDir: './tests', // Define where to put tests
+
   /* Run tests in files in parallel */
   fullyParallel: true,
+
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
+
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+
   /* Opt out of parallel tests on CI. */
+  // Undefined = number of workers = number of cores in your system
   workers: process.env.CI ? 1 : undefined,
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'https://demoqa.com/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    screenshot: 'only-on-failure'
   },
+
+  outputDir: 'test-results', // Folder for test artifacts like screenshots, videos, traces, etc.
 
   /* Configure projects for major browsers */
   projects: [
@@ -47,6 +57,30 @@ export default defineConfig({
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
+    },
+
+    {
+      name: 'all-browsers-and-tests',
+      use: {
+        baseURL: 'https://demoqa.com/',
+        ...devices['Desktop Chrome']
+      },
+    },
+
+    {
+      name: 'all-browsers-and-tests',
+      use: {
+        baseURL: 'https://demoqa.com/',
+        ...devices['Desktop Firefox']
+      },
+    },
+
+    {
+      name: 'all-browsers-and-tests',
+      use: {
+        baseURL: 'https://demoqa.com/',
+        ...devices['Desktop Safari']
+      },
     },
 
     /* Test against mobile viewports. */
